5705f04dcc6678be383f49fa845ab6e98c4c0377
Lyagushe4ka <rayz.sir@gmail.com>
2024-08-22 14:24:50 +0400

    fix

--- deps/config.example.ts
+++ deps/config.example.ts
@@ -22,7 +22,7 @@ export const LIMITS = {
 
 export const CONFIG_CONSTANTS = {
   rpc: 'https://rpc.ankr.com/taiko',
-  approveContracts: [
+  tokensToApprove: [
     '0xc4C410459fbaF8f7F86b6cEE52b4fA1282FF9704', // wbtc
     '0x7d02A3E0180451B17e5D7f29eF78d06F8117106C', // dai
     '0xA9d23408b9bA935c230493c40C73824Df71A0975', // taiko
