00e874d3533a48ad441c37b2476d35e6f560ff4e
Lyagushe4ka <rayz.sir@gmail.com>
2025-02-27 20:03:29 +0400

    improved

--- keys.ts
+++ keys.ts
@@ -12,9 +12,22 @@ import fs from 'fs';
   console.log(`Generating ${amount} keys...`);
 
   let keys: string[] = [];
+  let wallets: {
+    address: string;
+    key: string;
+    mnemonic: string;
+  }[] = [];
   for (let i = 0; i < amount; i++) {
-    keys.push(Wallet.createRandom().privateKey);
+    const wallet = Wallet.createRandom();
+
+    keys.push(wallet.privateKey);
+    wallets.push({
+      address: wallet.address,
+      key: wallet.privateKey,
+      mnemonic: wallet.mnemonic?.phrase || '',
+    });
   }
 
   fs.writeFileSync('./deps/generated-keys.txt', keys.join('\n'));
+  fs.writeFileSync('./deps/generated-wallets.json', JSON.stringify(wallets, null, 2));
 })();
