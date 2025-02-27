b29c8720ef1d01c6a04426ca363ea3df7662fdf9
Lyagushe4ka <rayz.sir@gmail.com>
2025-02-27 19:50:17 +0400

    fix

--- keys.ts
+++ keys.ts
@@ -4,6 +4,13 @@ import fs from 'fs';
 (() => {
   const amount = parseInt(process.env.AMOUNT!);
 
+  if (isNaN(amount)) {
+    console.error('Invalid AMOUNT provided.');
+    process.exit(1);
+  }
+
+  console.log(`Generating ${amount} keys...`);
+
   let keys: string[] = [];
   for (let i = 0; i < amount; i++) {
     keys.push(Wallet.createRandom().privateKey);
